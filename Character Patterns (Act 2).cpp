9a2200268acdc33db9f6e5d746c78ed67d7eb075
Naalla Venkateshwarlu <114277372+W-A-R-L-U@users.noreply.github.com>
2023-05-27 18:35:44 +0530

    Create Character Patterns (Act 2).cpp

---
+++ Character Patterns (Act 2).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string testcases;
+	cin>>testcases;
+	for(int t=stoi(testcases);t>0;t--)
+	{
+		int row;
+		int col;
+		cin>>row;
+		cin>>col;
+		for(int i=0;i<row;i++)
+		{
+			for(int j=0;j<col;j++)
+			{
+				if(i==0 || i==row-1) cout<<"*";
+				else
+				{
+					if(j==0 || j==col-1) cout<<"*";
+					else cout<<".";
+				}
+			}
+			cout<<endl;
+		}
+	}
+	return 0;
+}
