ae783447066f7e927d0d77e7580d508dc4995323
Naalla Venkateshwarlu <114277372+W-A-R-L-U@users.noreply.github.com>
2023-05-27 18:17:27 +0530

    Create EXPECT.cpp

---
+++ EXPECT.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	while(true)
+	{
+		int number;
+		cin>>number;
+		cout<<number;
+		cout<<endl;
+		fflush(stdout);
+		if(number==42) break;
+	}
+	return 0;
+}
